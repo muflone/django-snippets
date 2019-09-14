c0dea3770e38656104d51158bf398fff5fdb8f81
Muflone <webreg@vbsimple.net>
2019-09-14 18:05:06 +0200

    First commit

---
+++ project/urls.py
@@ -0,0 +1,54 @@
+##
+#     Project: Django Snippets
+# Description: A Django application to organize Code snippets
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2019 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.urls import path
+
+
+# Define routes
+urlpatterns = []
+
+# Add i18n prefixes for sections
+urlpatterns += i18n_patterns(
+    path(settings.ADMIN_URL, admin.site.urls),
+    prefix_default_language=True
+)
+
+# Add standard paths
+urlpatterns = [
+    path(settings.ADMIN_URL, admin.site.urls),
+]
